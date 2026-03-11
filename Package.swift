dbe67a95ee52a00525fb3a056e05193a7b71bb74
Wayne Burkett <wayne.burkett@verint.com>
2026-03-11 16:33:12 -0400

    Fix platform version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "VerintXM",
     platforms: [
-        .iOS(.v13)
+        .iOS(.v12)
     ],
     products: [
         .library(name: "VerintXM", targets: ["EXPSurveyManagement", "EXPCore", "EXPPredictive", "EXPIngestionLogger", "EXPUtilities"])
